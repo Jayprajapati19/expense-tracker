fc149f88f19538ee7f2ed6060f1947f64ddcdc7d
JayPrajapati19 <jayp37679@gmail.com>
2024-06-24 16:45:41 +0530

    create sum and sql qouery

--- app/(routes)/dashboard/budgets/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetList.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 import CreatedBudget from './CreatedBudget'
+import { db } from '@/utils/dbConfig'
+import { eq, getTableColumns, sql } from 'drizzle-orm'
+import { Budgets, Expenses } from '@/utils/schema'
 
 function BudgetList() {
+
+    //  used to get budget list
+
+
+    const getBudgetList = async () => {
+        const result = await db.select({
+            ...getTableColumns(Budgets),
+            totalSpend: sql`sum(${Expenses.amount})`.mapWith(Number),
+            totalItem: sql`count(${Expenses.id})`.mapWith(Number)
+        }).from(Budgets)
+            .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+            .groupBy(Budgets.id);
+
+        console.log(result);
+    }
+
     return (
         <div className='mt-7'>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
